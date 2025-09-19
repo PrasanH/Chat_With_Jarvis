ccbf1b18fbb0ae4cdef6d1cdb2104197750875a9
PrasanH <prasanna99h@gmail.com>
2025-09-19 11:24:35 +0200

    updated llm_utils

--- project/app_utils/llm_utils.py
+++ project/app_utils/llm_utils.py
@@ -49,10 +49,13 @@ def get_text_from_documents(uploaded_docs):
             pdf_reader = PdfReader(uploaded_doc)
             for page in pdf_reader.pages:
                 text += page.extract_text()
+
         elif ".docx" in uploaded_doc.name:
             docx_text = extract_text_from_docx(uploaded_doc)
             text += docx_text
+
     return text
+
 
 
 def extract_text_from_docx(docx_file):
@@ -72,6 +75,7 @@ def extract_text_from_docx(docx_file):
         text += paragraph.text + "\n"
 
     return text
+
 
 
 def get_text_chunks(raw_text):
