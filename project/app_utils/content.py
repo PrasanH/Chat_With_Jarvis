9cddc081f79b6e65c4cad7de0a2a473f9f7f84d3
PrasanH <prasanna99h@gmail.com>
2025-10-22 17:34:10 +0200

    separated content into separate file

---
+++ project/app_utils/content.py
@@ -0,0 +1,30 @@
+
+
+pre_defined_content = [
+    "You are an Intelligent assistant who is good at explaining things in a simple way",
+
+    "You are an Intelligent assistant who is good at explaining things in a simple way. Answer upto point. Do not bullshit", 
+    #for GPT5 based models
+    
+    "Explain in simple words as if explaining it to a child",
+
+    "Please provide me the keywords from the text, which I can include in my resume. If it's in German, give the same german keywords",
+
+    "From the job description, tell me what are some qualities that this company is looking for?",
+
+    "From the job description, mention some of the points to highlight in my cover letter.",
+
+    "You are an expert in programming",
+
+    "Answer in minimum words as possible",
+
+    "Answer in minimum words as possible with reasoning",
+
+    "Check the grammar and rephrase if required. You are also allowed to improvise",
+
+    "I will give word(s). Just return suitable emojis and nothing else.",
+
+    "Explain in simple words in Hinglish. Maintain a friendly tone. keep the text in english",
+
+    "Explain in simple words in Kannada-English. Maintain a friendly tone. keep the text in english",
+]
